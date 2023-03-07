fd51320dc7fa12a30b089e140c11739741f67493
mfm051 <mateusmatos051@gmail.com>
2023-03-07 20:28:57 -0300

    Add basic calculator functionality)

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const divide = (a, b) => (b === 0)? 'hahaha...' : a / b;
-const multiply = (a, b) => a * b;
+const calculator = {
+    a:          false,
+    b:          false,
+    operation:  false,
+    add:        function() {return this.a + this.b},
+    subtract:   function() {return this.a - this.b},
+    divide:     function() {return this.b === 0 ? 'mathError' : this.a / this.b},
+    multiply:   function() {return this.a * this.b},
+    getResult:  function() {
+                            if      (this.operation==='add')        return this.add()
+                            else if (this.operation==='subtract')   return this.subtract()
+                            else if (this.operation==='divide')     return this.divide()
+                            else                                    return this.multiply()},
+    refresh:    function() {this.a = false;
+                            this.b = false; 
+                            this.operation = false},
+}
 
+document.querySelectorAll(".numbers > input[type='button']").forEach(
+    button => button.addEventListener("mousedown", function(e) {
+        if (calculator.a === false) calculator.a = Number(e.target.id)
+        else { //If there's a number (a) already
+            calculator.b = Number(e.target.id);
+            console.log( calculator.getResult() );
+            calculator.refresh();
+        }
+    })
+);
+
+document.getElementById("+").addEventListener("mousedown", () => calculator.operation = 'add');
+document.getElementById("-").addEventListener("mousedown", () => calculator.operation = 'subtract');
+document.getElementById("*").addEventListener("mousedown", () => calculator.operation = 'multiply');
+document.getElementById("/").addEventListener("mousedown", () => calculator.operation = 'divide');
