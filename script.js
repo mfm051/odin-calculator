42ecaa0200f0eef4db3662eb68986ba80df90a9e
mfm051 <mateusmatos051@gmail.com>
2023-03-09 20:20:23 -0300

    Update pickNum method to permit more numbers

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ const calculator = {
     subtract()              {return this.a - this.b},
     divide()                {return this.b === 0 ? 'mathError' : this.a / this.b },
     multiply()              {return this.a * this.b},
-    pickNum(num)            {this.chosenOperation === false ? this.a = num : this.b = num},
+    pickNum(num)            {
+                                if (this.chosenOperation === false) {
+                                    this.a === false ? this.a = num : this.a = Number (`${this.a}${num}`)
+                                }
+                                else {
+                                    this.b === false ? this.b = num : this.b = Number (`${this.b}${num}`)
+                                }
+                            },
     pickOperator(operator)  {this.chosenOperation = operator; if (this.a === false) this.a = 0},
     getResult()             {   
                                 if (this.chosenOperation === false)         return
@@ -21,16 +28,19 @@ const calculator = {
                                 if (this.chosenOperation === 'multiply')    return this.multiply()
                                 if (this.chosenOperation === 'divide')      return this.divide()
                             },
-    clearLast()             {   
-                                if (this.b !== false){
+    removeLast()             {   
+                                if (this.b !== false) {
                                     let numToString = this.b.toString();
                                     if (numToString.length > 1) this.b = Number(numToString.slice(0, numToString.length - 1))
+                                    else this.b = false
                                 }
                                 else if (this.chosenOperation !== false) this.chosenOperation = false
-                                else {
+                                else if (this.a !== false) {
                                     let numToString = this.a.toString();
                                     if (numToString.length > 1) this.a = Number(numToString.slice(0, numToString.length - 1))
-                                }            
+                                    else this.a = false
+                                }
+                                else return
                             },
     clearAll()              {
                                 this.a = false;
@@ -44,12 +54,16 @@ calculator.numbers.forEach (
         "mousedown",
         (e) => calculator.pickNum(Number(e.target.id))
     )
-)
+);
 
 calculator.operations.forEach (
     operation => operation.addEventListener (
         "mousedown",
         (e) => calculator.pickOperator(e.target.id)
     )
-)
+);
 
+calculator.backSpace.addEventListener (
+    "mousedown",
+    () => calculator.removeLast()
+);
