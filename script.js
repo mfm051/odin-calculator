cf1e7878c52cc326114343a0ce1cf96967a1b05b
mfm051 <mateusmatos051@gmail.com>
2023-03-09 18:49:52 -0300

    Add event listeners for updating numbers and operator

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ const calculator = {
     multiply()              {return this.a * this.b},
     pickNum(num)            {this.chosenOperation === false ? this.a = num : this.b = num},
     pickOperator(operator)  {this.chosenOperation = operator; if (this.a === false) this.a = 0},
-    getResult()             {   if (this.chosenOperation === false)         return
+    getResult()             {   
+                                if (this.chosenOperation === false)         return
                                 if (this.chosenOperation === 'add')         return this.add()
                                 if (this.chosenOperation === 'subtract')    return this.subtract()
                                 if (this.chosenOperation === 'multiply')    return this.multiply()
                                 if (this.chosenOperation === 'divide')      return this.divide()
                             },
-    clearLast()             {   if (this.b !== false){
+    clearLast()             {   
+                                if (this.b !== false){
                                     let numToString = this.b.toString();
                                     if (numToString.length > 1) this.b = Number(numToString.slice(0, numToString.length - 1))
                                 }
@@ -27,5 +29,19 @@ const calculator = {
                                     let numToString = this.a.toString();
                                     if (numToString.length > 1) this.a = Number(numToString.slice(0, numToString.length - 1))
                                 }            
-                            }
+                            },
 };
+
+calculator.numbers.forEach (
+    number => number.addEventListener (
+        "mousedown",
+        (e) => calculator.pickNum(Number(e.target.id))
+    )
+)
+
+calculator.operations.forEach (
+    operation => operation.addEventListener (
+        "mousedown",
+        (e) => calculator.pickOperator(e.target.id)
+    )
+)
