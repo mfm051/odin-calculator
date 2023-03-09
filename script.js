3d4c7580e8448936ffad9a97c6c21886ab043fbb
mfm051 <mateusmatos051@gmail.com>
2023-03-08 21:32:04 -0300

    Reformulate script

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
 const calculator = {
-    a:          false,
-    b:          false,
-    operation:  false,
-    add:        function() {return this.a + this.b},
-    subtract:   function() {return this.a - this.b},
-    divide:     function() {return this.b === 0 ? 'mathError' : this.a / this.b},
-    multiply:   function() {return this.a * this.b},
-    getResult:  function() {
-                            if      (this.operation==='add')        return this.add()
-                            else if (this.operation==='subtract')   return this.subtract()
-                            else if (this.operation==='divide')     return this.divide()
-                            else                                    return this.multiply()},
-    refresh:    function() {this.a = false;
-                            this.b = false; 
-                            this.operation = false},
-}
+    numbers:    document.querySelectorAll(".numbers > input"),
+    actions:    document.querySelectorAll(".actions > input"),
+    add()       {return this.a + this.b},
+    subtract()  {return this.a - this.b},
+    divide()    {return this.b === 0 ? 'mathError' : this.a / this.b },
+    multiply()  {return this.a * this.b},
+};
 
-document.querySelectorAll(".numbers > input").forEach(
-    button => button.addEventListener("mousedown", function(e) {
-        if (calculator.a === false) calculator.a = Number(e.target.id)
-        else { //If there's a number (a) already
-            calculator.b = Number(e.target.id);
-            console.log( calculator.getResult() );
-            calculator.refresh();
-        }
-    })
-);
-
-document.getElementById("+").addEventListener("mousedown", () => calculator.operation = 'add');
-document.getElementById("-").addEventListener("mousedown", () => calculator.operation = 'subtract');
-document.getElementById("*").addEventListener("mousedown", () => calculator.operation = 'multiply');
-document.getElementById("/").addEventListener("mousedown", () => calculator.operation = 'divide');
+calculator.actions.forEach(
+  () => addEventListener("mousedown", 
+    function(e) {
+        if (e.target.id === 'add')      calculator.result = calculator.add();
+        if (e.target.id === 'subtract') calculator.result = calculator.subtract();
+        if (e.target.id === 'divide')   calculator.result = calculator.divide();
+        if (e.target.id === 'multiply') calculator.result = calculator.multiply();
+    }
+  )
+)
